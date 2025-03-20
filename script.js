4789136f1e886b2c798c3701b2e9241181adfa44
Florian Allard <76995062+FlorianAllard@users.noreply.github.com>
2025-03-20 15:49:33 +0100

    Added data.json & parsing of data to instantiate and fill templates for projects

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 "use strict";
+var projectData;
+fetch("/assets/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    projectData = data.projects;
+  })
+  .then(() => {
+    instantiateProjects();
+  });
 
 // GET BROWSER SCROLLBAR WIDTH
 const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
@@ -7,7 +16,7 @@ document.documentElement.style.setProperty(
   `${scrollbarWidth}px`
 );
 
-// SCROLL FEEDBACK
+//#region SCROLL FEEDBACK
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("#navigation a hr");
 
@@ -29,61 +38,104 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+//#endregion
 
-// PROJECT OVERLAY
+//#region PROJECT OVERLAY
+
 const overlay = document.querySelector("#project-overlay");
+
 overlay.addEventListener("click", (e) => {
   if (e.target == overlay) {
     closeOverlay();
   }
 });
-overlay.children[0];
-
 const closeButton = document.querySelector("#project-overlay button");
+console.log(closeButton);
 closeButton.addEventListener("click", closeOverlay);
-
-function openOverlay(id) {
-  overlay.classList.add("active");
-  overlay.scrollTo(0, 0);
-
-  const title = overlay.querySelector("h1");
-  switch (id.toLowerCase()) {
-    case "oversoc":
-      title.textContent = "OverSOC";
-      break;
-    case "notaspyware":
-      title.textContent = "Not A Spyware";
-      break;
-    case "cryptide":
-      title.textContent = "Cryptide";
-      break;
-    case "cubekingdoms":
-      title.textContent = "Cube Kingdoms";
-      break;
-    case "horrorfactory":
-      title.textContent = "Horror Factory";
-      break;
-    case "candlelightvr":
-      title.textContent = "Candlelight VR";
-      break;
-    case "maximtherobot":
-      title.textContent = "Maxim the Robot";
-      break;
-    case "meadgard":
-      title.textContent = "Meadgard";
-      break;
-    default:
-      break;
-  }
-}
 
 function closeOverlay() {
   overlay.classList.remove("active");
 }
 
-const projects = document.querySelectorAll(".project");
-projects.forEach((project) => {
-  project.addEventListener("click", function () {
-    openOverlay(this.id);
+const overlayContent = overlay.querySelector(".content");
+const overlayContentHtml = overlayContent.innerHTML;
+
+function openOverlay(currentProjectData) {
+  overlay.classList.add("active");
+  overlayContent.innerHTML = overlayContentHtml;
+  overlay.scrollTo(0, 0);
+
+  const title = overlay.querySelector("h1");
+  title.textContent = currentProjectData.title;
+
+  currentProjectData.overlayContent.forEach((a) => {
+    var newElement;
+
+    switch (a.type) {
+      case "img":
+        newElement = document.createElement("img");
+        newElement.setAttribute("src", a.src);
+        newElement.setAttribute("alt", a.alt);
+        if (a.modifier) {
+          newElement.classList.add(a.modifier);
+        }
+        overlayContent.appendChild(newElement);
+        break;
+
+        case "a":
+          newElement = document.createElement("a");
+          newElement.textContent = a.href;
+          newElement.setAttribute("href", a.href);
+          newElement.setAttribute("target", "_blank");
+          overlayContent.appendChild(newElement);
+          break;
+
+      default:
+        a.content.forEach((string) => {
+          newElement = document.createElement(a.type);
+          newElement.innerHTML = string;
+          overlayContent.appendChild(newElement);
+        });
+        break;
+    }
   });
-});
+}
+
+//#endregion
+
+//#region PROJECT TEMPLATE
+function instantiateProjects() {
+  const projectTemplate = document.querySelector("#project-template");
+
+  projectData.forEach((data) => {
+    let projectClone = projectTemplate.content.cloneNode(true);
+    const project = projectClone.querySelector(".project");
+    projectTemplate.parentNode.appendChild(projectClone);
+
+    project.id = data.id;
+    project.addEventListener("click", function () {
+      openOverlay(data);
+    });
+
+    const imgArray = project.querySelectorAll("img");
+    imgArray.forEach((img) => {
+      img.setAttribute("src", data.cover.src);
+      img.setAttribute("alt", data.cover.alt);
+    });
+
+    const title = project.querySelector("h4");
+    title.textContent = data.title;
+
+    const desc = project.querySelector("p");
+    desc.textContent = data.desc;
+
+    const chipTemplate = project.querySelector("#chip-template");
+    data.chips.forEach((chipData) => {
+      let chipClone = chipTemplate.content.cloneNode(true);
+      const chip = chipClone.querySelector(".chip");
+      chip.textContent = chipData;
+      chipTemplate.parentNode.appendChild(chipClone);
+    });
+  });
+}
+//#endregion
